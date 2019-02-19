8d6367a16f409c091f8b62a803d2d07e830a77dc
Yining Xu <yining.xu@linkplay.com>
2019-02-19 11:42:59 +0800

    [JS-Demo] 导航吸顶、侧边栏广告

---
+++ (JS)进阶/11-中部导航吸顶/js/myFunc.js
@@ -0,0 +1,31 @@
+/**
+ * 获取滚动的头部距离和左边距离
+ * @returns {{top: number, left: number}}
+ */
+function scroll() {
+    if (window.pageYOffset !== null) {
+        return{
+            top:window.pageYOffset,
+            left:window.pageXOffset
+        }
+    }else if (document.compatMode === 'CSS1Compat') { // W3C 标准模式
+        return {
+            top: document.documentElement.scrollTop,
+            left: document.documentElement.scrollLeft
+        }
+    }
+
+    return{
+        top: document.body.scrollTop,
+        left: document.body.scrollLeft
+    }
+}
+
+/**
+ * 根据id获取标签
+ * @param id
+ * @returns {any}
+ */
+function $(id) {
+    return typeof id === 'string' ? document.getElementById(id):null;
+}
